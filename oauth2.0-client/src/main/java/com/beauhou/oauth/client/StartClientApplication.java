8dd11c83355bfd2274e712932d4de5fb5a2f2f07
houbo <beauhou@qq.com>
2023-06-15 16:11:37 +0800

    项目初始化

---
+++ oauth2.0-client/src/main/java/com/beauhou/oauth/client/StartClientApplication.java
@@ -0,0 +1,14 @@
+package com.beauhou.oauth.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author BeauHou
+ */
+@SpringBootApplication
+public class StartClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StartClientApplication.class, args);
+    }
+}
---
+++ oauth2.0-server/src/main/java/com/oauth/server/StartServerApplication.java
@@ -0,0 +1,14 @@
+package com.oauth.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author BeauHou
+ */
+@SpringBootApplication
+public class StartServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StartServerApplication.class, args);
+    }
+}
